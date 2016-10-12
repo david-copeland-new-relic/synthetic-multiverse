cc54bf8ca55ff8cb130b505c831a6e136afa806e
Blaine Schmeisser <blainesch@gmail.com>
2016-10-11 19:27:05 -0700

    Remove commented webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,6 @@
 var webpack = require('webpack')
 var path = require('path')
 
-/*
-module.exports = {
-  context: __dirname + '/app',
-  entry: [
-    'webpack/hot/only-dev-server',
-    './js/app.js'
-  ],
-  output: {
-    path: __dirname + '/app/build',
-    filename: 'bundle.js',
-    publicPath : './app/'
-  },
-  module: {
-    loaders: [
-      { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.css$/, loader: 'style!css' }
-    ]
-  }
-}
-*/
-
 module.exports = {
   entry: {
     app: ['./app/js/app.js']
