4454b971490d4f45b92b2c9515ef987999a1cd3d
Blaine Schmeisser <blainesch@gmail.com>
2016-10-11 18:52:12 -0700

    Use relative pathing.

--- app/js/components/error.js
+++ app/js/components/error.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export class Error extends Component {
   render() {
     if (window.newrelic) {
-      window.newrelic.noticeError(new Error('Failed to load resource'))
+      try {
+        throw new Error('Failed to load resource')
+      } catch(e) {
+        window.newrelic.noticeError(e)
+      }
     }
     return <span>We threw an error!</span>
   }
