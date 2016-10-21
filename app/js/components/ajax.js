df140f6633592ec6a4c031676359982ba901ac2c
Blaine Schmeisser <blainesch@gmail.com>
2016-10-21 13:48:49 -0700

    Add better ajax requests.

--- app/js/components/ajax.js
+++ app/js/components/ajax.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react'
-import fetch from 'isomorphic-fetch'
+
+function retrieve(url) {
+  return new Promise((resolve) => {
+    var oReq = new XMLHttpRequest()
+    oReq.addEventListener('load', function reqListener () {
+      resolve(this.responseText)
+    })
+    oReq.open('GET', url)
+    oReq.send()
+  })
+}
 
 export class Ajax extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      image: '',
+      images: [],
     }
   }
 
   componentWillMount () {
-    fetch('./giphy.json')
-      .then((response) => response.json())
-      .then((json) => json.data[0].images.original.url)
-      .then((image) => {
-        this.setState({
-          image,
-        })
+    const pages = ['./giphy1.json', './giphy2.json', './giphy3.json'].sort(() => {
+      return 0.5 - Math.random()
+    })
+    retrieve(pages[0])
+      .then((response) => JSON.parse(response))
+      .then((json) => json.data[0].url)
+      .then((first) => {
+        retrieve(pages[1])
+          .then((response) => JSON.parse(response))
+          .then((json) => json.data[0].url)
+          .then((second) => {
+            retrieve(pages[2])
+              .then((response) => JSON.parse(response))
+              .then((json) => json.data[0].url)
+              .then((third) => {
+                this.setState({
+                  images: [first, second, third]
+                })
+              })
+          })
       })
   }
 
   render() {
-    return this.state.image ? (
-      <img src={this.state.image} />
-    ) : (
-      null
+    return (
+      <div className='images'>
+        { this.state.images.map((image) => {
+          return <img src={image} />
+        }) }
+      </div>
     )
   }
 }
