237f426831f48ab4d5819b38a02d65a77f6d2342
Blaine Schmeisser <blainesch@gmail.com>
2016-10-26 09:50:27 -0700

    Updates

--- js/components/links.js
+++ js/components/links.js
@@ -9,11 +9,15 @@ export class Links extends Component {
           <li><a target='_blank' href='https://staging.newrelic.com/accounts/550352/browser/575530'>Pro Staging</a></li>
           <li><a target='_blank' href='https://staging.newrelic.com/accounts/835096/browser/575591'>Lite Staging</a></li>
           <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1/browser/27679902'>Pro Production</a></li>
-          <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1449208/browser/27679991'>Lite Production</a></li>
+          <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1461294/browser/27274125'>Lite Production</a></li>
         </ul>
         <h3>Synthetics</h3>
         <ul>
           <li><a target='_blank' href='https://staging-synthetics.newrelic.com/accounts/550352/monitors/88690821-04cc-4694-9e10-d8e47ec1dd9a'>Synthetics</a></li>
+        </ul>
+        <h3>GitHub</h3>
+        <ul>
+          <li><a target='_blank' href='https://github.com/blainesch/synthetic-multiverse'>Repo</a></li>
         </ul>
       </div>
     )
