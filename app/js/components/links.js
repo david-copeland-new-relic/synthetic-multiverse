ffb2027ea1e735c7090a8aab929c36efe8e7bfa2
Blaine Schmeisser <blainesch@gmail.com>
2016-10-26 09:49:59 -0700

    Update to a real lite account.

--- app/js/components/links.js
+++ app/js/components/links.js
@@ -9,7 +9,7 @@ export class Links extends Component {
           <li><a target='_blank' href='https://staging.newrelic.com/accounts/550352/browser/575530'>Pro Staging</a></li>
           <li><a target='_blank' href='https://staging.newrelic.com/accounts/835096/browser/575591'>Lite Staging</a></li>
           <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1/browser/27679902'>Pro Production</a></li>
-          <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1449208/browser/27679991'>Lite Production</a></li>
+          <li><a target='_blank' href='https://rpm.newrelic.com/accounts/1461294/browser/27274125'>Lite Production</a></li>
         </ul>
         <h3>Synthetics</h3>
         <ul>
