c0f1c5da12a5be42d38990d584a1bedee3b8f952
Amjith Ramanujam <amjith.r@gmail.com>
2016-11-07 11:47:55 -0800

    Don't catch the error on the error page.

--- app/js/components/error.js
+++ app/js/components/error.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react'
 export class Error extends Component {
   render() {
     if (window.newrelic) {
-      try {
-        throw new Error('Failed to load resource')
-      } catch(e) {
-        window.newrelic.noticeError(e)
-      }
+      throw new Error('Failed to load resource')
     }
     return <span>We threw an error!</span>
   }
