23472176b6f388bfbb34447763dc1d903cea5a2f
Amjith Ramanujam <amjith.r@gmail.com>
2016-11-07 11:25:31 -0800

    Add sourcemap support.

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,28 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
+  devtool: 'source-map',
   entry: {
-    app: ['./app/js/app.js']
+    app: ['./app/js/app.js'],
   },
   output: {
     path: path.resolve(__dirname, 'app', 'assets'),
     publicPath: '/assets/',
-    filename: 'bundle.js'
+    filename: 'bundle.min.js',
+    sourceMapFilename: 'bundle.map.js',
   },
   module: {
     loaders: [
       { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-      { test: /\.css$/, loader: 'style!css' }
-    ]
-  }
-};
+      { test: /\.css$/, loader: 'style!css' },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
+}
