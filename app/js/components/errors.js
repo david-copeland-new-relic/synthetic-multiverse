16639d32928d4d641a7c1a225e1e79ee4b987d85
Blaine Schmeisser <blainesch@gmail.com>
2016-11-08 11:41:43 -0800

    Catch error.

--- app/js/components/errors.js
+++ app/js/components/errors.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 
 export class Errors extends Component {
   render() {
-    throw new Error('Whoops!')
+    try {
+      throw new Error('Whoops!')
+    } catch (e) {
+      newrelic.noticeError(e)
+    }
     return <span>We threw an error!</span>
   }
 }
