dbf0581a70b249df24c063a97564d7731fafe4a8
Blaine Schmeisser <blainesch@gmail.com>
2016-11-18 11:39:10 -0800

    Updates

--- js/components/slow.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export class Slow extends Component {
-  render() {
-    const start = new Date()
-    while (new Date() - start < 1500);
-    return <span>I render slowly...</span>
-  }
-}
